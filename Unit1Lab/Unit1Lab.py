02763b012080372df88b8af28b2a06f75f4497e6
Gabriel Chargualaf <20ChargualafGabriel@bprep.org>
2018-09-07 09:16:39 -0700

    Unit 1 Lab

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,36 @@
+#print ('I Love Python, what is your name ')
+
+#myName = input()
+
+#print (myName)
+
+#print ('my name is ' + (myName))
+
+
+#print (4+3)
+#print (10*3)
+#print (75-12)
+#print (18%2)
+#print (19%2)
+#print (15/4)
+
+print ("Enter Number 1")
+num1 = input()
+print ('Enter Number 2')
+num2 = input()
+
+print ('Your numbers added together equal')
+#print (num1)
+#print (num2)
+
+print (num1 + num2)
+
+print ('But in traditional math would equal ')
+print (int(num1) + int(num2))
+
+intA = 25
+intB = 25
+intC = 35
+
+print (intA == intB)
+

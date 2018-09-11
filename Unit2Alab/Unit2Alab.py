ffd7647bd28378b98ecfcf38d0f201cf920fa253
Gabriel Chargualaf <20ChargualafGabriel@bprep.org>
2018-09-11 08:29:45 -0700

    Unit2 Lesson A Lab

---
+++ Unit2Alab/Unit2Alab.py
@@ -0,0 +1,28 @@
+#print (5 + 4)
+#print (5.5 + 7.0)
+
+#print (IntA + IntB)
+
+
+#input
+#print (input + IntA)
+
+#num1 = (input)
+#print (num1)
+#num2 = (input)
+#print (num2)
+#print (num1 + num2)
+#print (int(num1) + int(num2))
+
+intI = 100
+intJ = 200
+intX = 300
+intY = 100
+
+print ((intI == intJ) or (intI > intX))
+
+print ((intI < intJ) and (intI >= intY))
+
+print ((intI != intJ) and (intI != intX))
+
+print ((intI < intJ) and (intI < intX) and (intI < intY))

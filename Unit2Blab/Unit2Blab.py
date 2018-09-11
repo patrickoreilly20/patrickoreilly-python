c7c9bf5266632d1d4bd95028cd1f9a569cc0d04d
Gabriel Chargualaf <20OreillyPatrick@bprep.org>
2018-09-11 09:07:42 -0700

    Unit 2 Lesson B

---
+++ Unit2Blab/Unit2Blab.py
@@ -0,0 +1,34 @@
+#numGrade = int(input())
+#print (numGrade)
+#
+#if numGrade > 90:
+#   print ("Yahoo you got an A")
+#
+#elif numGrade >= 80:
+#
+#        print ("Good you got a B")
+#
+#elif numGrade >= 70:
+#
+#        print ("okay you got a C")
+#
+#else:
+ #   print ("better study harder")
+
+print ('What is your favorite color')
+userInput = input()
+#print (userInput)
+
+if userInput == 'blue':
+    print ('The sky is blue')
+
+elif userInput == 'green':
+    print ('The Forest is green')
+
+elif userInput == 'red':
+    print ('I love red')
+
+elif userInput == 'yellow':
+    print ('Yuck Yellow')
+
+else: print ('I am a dumb computer, I only know 4 colors')

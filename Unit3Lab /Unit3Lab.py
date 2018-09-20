010cf24ef690dbb76a903d26723cea921bdaebb6
Gabriel Chargualaf <20OreillyPatrick@bprep.org>
2018-09-20 08:57:31 -0700

    Lab

--- Unit3Lab /Unit3Lab.py
+++ Unit3Lab /Unit3Lab.py
@@ -9,34 +9,34 @@ def schoolLen() :
     yearsSchool = (int(years) - 1)
     print('you have been in school ' +str(yearsSchool) + ' years')
 
-schoolLen()
+#schoolLen()
 
 
-def homeBase() :
-    home = input ('What city are you from - ')
-    schoolYears = input ('How  many years have you been in school - ')
-    print ('You are from ' +str(home))
-    print ('You have been in school for ' +str(schoolYears) + ' years')
+#def homeBase() :
+    #home = input ('What city are you from - ')
+   # schoolYears = input ('How  many years have you been in school - ')
+  #  print ('You are from ' +str(home))
+ #   print ('You have been in school for ' +str(schoolYears) + ' years')
 
-homeBase()
+#homeBase()
 
 from random import *
 
-def randomNumber():
-    x = input('What is your start value - ')
-    y = input('What is your ending value - ')
-    myNumber = randint(int(x) , int(y))
-    print(str(myNumber) + ' a random number between ' +str(x) + ' and ' +str(y))
+#def randomNumber():
+    #x = input('What is your start value - ')
+   # y = input('What is your ending value - ')
+  #  myNumber = randint(int(x) , int(y))
+ #   print(str(myNumber) + ' a random number between ' +str(x) + ' and ' +str(y))
 
-randomNumber()
+#randomNumber()
 
-def boxArea():
-    x = input('What is the length of your box in centimeters - ')
-    y = input('what is the width of your box in centimeters - ')
-    area = int(x) * int(y)
-    print('The area of your box is ' +str(area) + ' centimeters^2')
+#def boxArea():
+    #x = input('What is the length of your box in centimeters - ')
+   # y = input('what is the width of your box in centimeters - ')
+  #  area = int(x) * int(y)
+ #   print('The area of your box is ' +str(area) + ' centimeters^2')
 
-boxArea()
+#boxArea()
 
 def boxPerim():
     x = input('What is the length of your box in centimeters - ')
@@ -44,4 +44,6 @@ def boxPerim():
     perim = int(x) + int(x) + int(y) + int(y)
     return(perim)
 
-boxPerim()
+xperim = boxPerim()
+print('The perimeter is')
+print(xperim)

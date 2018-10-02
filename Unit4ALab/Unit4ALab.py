1adb698ca8c10ae3c9229df5558a05e7b197c40c
Gabriel Chargualaf <20OreillyPatrick@bprep.org>
2018-10-02 09:12:30 -0700

    Unit 4 Lab

---
+++ Unit4ALab/Unit4ALab.py
@@ -0,0 +1,21 @@
+def main():
+
+    strWord = input('Input a word here - ')
+
+    print(strWord)
+
+    answer = deVowel(strWord)
+
+def deVowel(mystring):
+
+    print(mystring)
+
+    novowels = ''
+
+    for letter in mystring:
+        if mystring = 
+
+
+
+
+main()

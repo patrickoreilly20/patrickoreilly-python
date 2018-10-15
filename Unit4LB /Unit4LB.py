c1e68aa6244882a57626618df2654ffaa30d1478
Gabriel Chargualaf <20OreillyPatrick@bprep.org>
2018-10-15 08:35:15 -0700

    Unit4C Lab

---
+++ Unit4LB /Unit4LB.py
@@ -0,0 +1,27 @@
+for i in range(0,5):
+
+    print(i)
+
+    answer = (i+10)
+
+    answr = (i*10)
+
+    print("pause")
+
+    print( str(i) + "+ 10 =" + str(answer))
+
+    print( str(i) + "*10 =" + str(answr))
+
+    print("pause")
+
+myList= [10,20,30,40,50]
+
+print(myList)
+
+for a in range(0, len(myList)):
+
+    list=[]
+
+    myList[a]=myList[a] *10
+
+    print(myList)
